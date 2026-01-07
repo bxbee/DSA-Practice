3432fa65925820f8e4664eed3b0dbd116592fcfc
bxbee <bxbeeraj@gmail.com>
2026-01-07 22:51:57 +0530

    Added code of map

---
+++ Basics/Char_counter_using_Map.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+int main() {
+    // Read size of the array
+    int n;
+    cin >> n;
+
+    // Read array elements
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Build frequency map
+    map<int, int> freq;
+    for (int x : arr) {
+        freq[x]++;
+    }
+
+    // Process queries
+    int t;
+    cin >> t;
+    while (t--) {
+        int number;
+        cin >> number;
+        cout << freq[number] << " ";
+    }
+
+    return 0;
+}
