55adb03c5c1c54798583ad5b06d392ac5c03e4d2
Raj Ghosh <148113238+bxbee@users.noreply.github.com>
2026-03-05 20:42:10 +0530

    Add multiple rotate array implementations
    Added multiple implementations of the rotate function for arrays, including optimal, brute force, and space-efficient methods.

--- LeetCode Solution/189.RotateArray.cpp
+++ LeetCode Solution/189.RotateArray.cpp
@@ -1,3 +1,4 @@
+//Optimal
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
@@ -10,5 +11,37 @@ public:
         reverse(nums.begin(), nums.begin() + k);
         // Step 3: Reverse remaining n-k elements
         reverse(nums.begin() + k, nums.end());
+    }
+};
+
+//brute
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %=n;
+        for(int i=0;i<n;i++){
+            int last = nums[n-1];
+            for(int j = n-1;j>0;j--){
+                nums[j] = nums[j-1];
+
+            }
+         nums[0] = last;
+
+        }
+    }
+};
+//By taking more sapce 
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+        vector<int> temp(n);
+        
+        for (int i = 0; i < n; i++) {
+            temp[(i + k) % n] = nums[i];
+        }
+        nums = temp;
     }
 };
