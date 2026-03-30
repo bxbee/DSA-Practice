cdf5d3df708d6fd0aae841c4172f4b90c771fb6f
Raj Ghosh <148113238+bxbee@users.noreply.github.com>
2026-03-30 21:55:39 +0530

    Implement multiple inheritance with Artist and Mathematician

--- Object-Oriented-Programming/06_multiple_inheritance.cpp
+++ Object-Oriented-Programming/06_multiple_inheritance.cpp
@@ -1,1 +1,35 @@
 //multi inheritance 
+#include <iostream>
+using namespace std;
+
+// Parent 1: Artistic traits
+class Artist {
+public:
+    void showArtSkill() {
+        cout << "I can paint beautiful landscapes 🎨" << endl;
+    }
+};
+
+// Parent 2: Mathematical traits
+class Mathematician {
+public:
+    void showMathSkill() {
+        cout << "I can solve complex equations ➗" << endl;
+    }
+};
+
+// Child inherits from BOTH parents
+class MultiTalented : public Artist, public Mathematician {
+public:
+    void showTalent() {
+        cout << "I am multi-talented, inheriting from both parents!" << endl;
+    }
+};
+
+int main() {
+    MultiTalented person;
+    person.showTalent();       // Child’s own method
+    person.showArtSkill();     // From Artist
+    person.showMathSkill();    // From Mathematician
+    return 0;
+}
