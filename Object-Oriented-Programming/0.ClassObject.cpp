0b021cfa1bf587dab512b6ec3ad5541f7ce6bd50
Raj Ghosh <148113238+bxbee@users.noreply.github.com>
2026-04-02 14:33:23 +0530

    Implement Teacher class with salary management
    Added Teacher class with attributes and methods for salary and department management.

--- Object-Oriented-Programming/0.ClassObject.cpp
+++ Object-Oriented-Programming/0.ClassObject.cpp
@@ -1,1 +1,51 @@
+#include <iostream>
+#include<string>
+using namespace std;
 
+class Teacher{
+  private:
+  double salary;
+  
+  public:
+  //properties //attribute
+  string name;
+  string dept;
+  string subject;
+  
+  
+  // methods // member function 
+  void changeDept(string newDept){
+    
+    dept = newDept;
+  }
+  void setSalary(double s){
+    salary = s;
+  }
+  double getSalary(){
+    return salary;
+  }
+  
+};
+class Student{
+  
+};
+
+int main() 
+{
+  Teacher t1;
+  Teacher t2;
+  Teacher t3;
+  Teacher t4;
+  Teacher t5;
+  t1.name = "Rex";
+  t1.subject = "Research";
+  t1.dept = "CSE";
+  
+  t1.setSalary(2500);
+  cout<<t1.getSalary();
+  
+  
+  
+    
+    return 0;
+}
