4d4fe0953b0f89dd39fcc04fcbd22ddf757916fd
Raj Ghosh <148113238+bxbee@users.noreply.github.com>
2026-05-04 17:55:56 +0530

    Update 7. Reverse Integer.cpp

--- LeetCode Solution/7. Reverse Integer.cpp
+++ LeetCode Solution/7. Reverse Integer.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int reverse(int x) {
         int rev = 0;
-        while(x!=0){
+        while(x!=0){ // and in this Loop minnor mistake
             int rem = x%10;
             
            
             x/=10;
             rev = rev*10+rem;
-            if(INT_MAX/10<rev || INT_MIN/10>rev){
+            if(INT_MAX/10<rev || INT_MIN/10>rev){ // IN revision I made herre Mistake 
                 return 0;
             }
             
